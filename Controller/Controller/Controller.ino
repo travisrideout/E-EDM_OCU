0339864d724fe9386ee439b6d5c44c3d489d4c73
travisrideout <travis.rideout518@gmail.com>
2016-10-12 16:24:15 -0400

    Project Setup

---
+++ Controller/Controller/Controller.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Controller.ino
+ Created:	10/12/2016 16:19:26
+ Author:	travis.rideout
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ Controller/Vehicle/Vehicle.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Vehicle.ino
+ Created:	10/12/2016 16:21:08
+ Author:	travis.rideout
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
